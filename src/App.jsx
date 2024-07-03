032c5c668bd2dd45fd0a9e949335d47c9b7161d0
“farah-jannat” <farahjannatmithila@gmail.com>
2024-07-03 21:50:15 +0600

    Added  nesessary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import Nevbar from './components/Nevbar'
-
+import Nevbar from "./components/Nevbar";
+import Sidebar from "./components/Sidebar";
+import Post from "./components/Post";
+import Rightbar from "./components/Rightbar";
 const App = () => {
   return (
     <>
-    <Nevbar />
+      <Nevbar />
+      <div className="container">
+        <div className="side-bar">
+          <Sidebar />
+        </div>
+        <div className="post">
+          <Post />
+        </div>
+        <div className="right-bar">
+          <Rightbar />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
