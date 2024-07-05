7e5b26a107be116cdb49c7ec42f93e393a9d091b
“farah-jannat” <farahjannatmithila@gmail.com>
2024-07-05 23:40:59 +0600

    Designed rightbar.jsx

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Rightbar = () => {
+const Rightbar = ({data, bgColor,color}) => {
   return (
     <div>
-      rightbar
+      <div className="right_container" style={{backgroundColor:`${bgColor}`, color:`${color}`}}>
+        <div className="post_img">
+          <img id='_img' src={data.img} alt="" />
+          <div className="inside_img">
+            <div className="img">
+              <img src={data.authorImg} alt="" />
+            </div>
+            <div className="text">
+              <h3>{data.authorName}</h3>
+              <p>{data.authorText}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
