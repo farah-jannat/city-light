a83808be84ad4d27d28891b8ba6ef67fd3da37c7
“farah-jannat” <farahjannatmithila@gmail.com>
2024-07-05 23:40:10 +0600

    Added toggle for darkmode

--- src/components/Nevbar.jsx
+++ src/components/Nevbar.jsx
@@ -1,18 +1,40 @@
 import { FcSearch } from "react-icons/fc";
 import { IoFilterSharp } from "react-icons/io5";
-const Nevbar = () => {
-  return (
-    <>
-      <div className="nav-bar">
-        <div className="logo">WDM</div>
-        <div className="search">
-            <div className="left"> <FcSearch /> Search here</div>
-            <div className="right"><IoFilterSharp/> filter</div>
-        </div>
-        <div className="seller">Become a seller</div>
-      </div>
-    </>
-  )
-}
+import { MdToggleOff } from "react-icons/md";
+import { MdToggleOn } from "react-icons/md";
 
-export default Nevbar
+const Nevbar = ({ bgColor, color, toggleButtun, toggle }) => (
+  <>
+    <div className="nav-bar">
+      <div
+        className="logo"
+        style={{ backgroundColor: `${bgColor}`, color: `${color}` }}
+      >
+        WDM
+        <div onClick={toggleButtun}>
+          {toggle ? (<MdToggleOn /> ): (<MdToggleOff />)}
+        </div>
+      </div>
+      <div
+        className="search"
+        style={{ backgroundColor: `${bgColor}`, color: `${color}` }}
+      >
+        <div className="left">
+          {" "}
+          <FcSearch /> Search here
+        </div>
+        <div className="right">
+          <IoFilterSharp /> filter
+        </div>
+      </div>
+      <div
+        className="seller"
+        style={{ backgroundColor: `${bgColor}`, color: `${color}` }}
+      >
+        Become a seller
+      </div>
+    </div>
+  </>
+);
+
+export default Nevbar;
