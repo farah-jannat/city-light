d2e50f982f7d8a93c0187d3b9d2b53491d32cbb8
“farah-jannat” <farahjannatmithila@gmail.com>
2024-07-05 23:39:15 +0600

    Added darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,49 @@ import Nevbar from "./components/Nevbar";
 import Sidebar from "./components/Sidebar";
 import Post from "./components/Post";
 import Rightbar from "./components/Rightbar";
+import { data, data2 } from "./Data";
+import { useState } from "react";
+
 const App = () => {
+  const [bgColor, setbgColor] = useState("blue");
+  const [color, setColor] = useState("white");
+  const [toggle, setToggle] = useState(false);
+
+  const toggleButtun = () => {
+    setToggle(!toggle);
+    if (toggle) {
+      setbgColor("gray");
+      setColor("black");
+      document.querySelector("body").style.backgroundColor = "black";
+    } else {
+      setbgColor("blue");
+      setColor("white");
+      document.querySelector("body").style.backgroundColor = "white";
+    }
+  };
+
+  // 232D3f
   return (
     <>
-      <Nevbar />
+      <Nevbar
+        bgColor={bgColor}
+        color={color}
+        toggleButtun={toggleButtun}
+        toggle={toggle}
+      />
       <div className="container">
         <div className="side-bar">
-          <Sidebar />
+          <Sidebar bgColor={bgColor} color={color} />
         </div>
         <div className="post">
-          <Post />
+          {data.map((d) => (
+            <Post key={d.id} data={d} bgColor={bgColor} color={color} />
+          ))}
         </div>
         <div className="right-bar">
-          <Rightbar />
+          {data2.map((d) => (
+            <Rightbar key={d.id} data={d} bgColor={bgColor} color={color} />
+          ))}
         </div>
       </div>
     </>
